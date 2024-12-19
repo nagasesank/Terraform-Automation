62dc26bc19c512de05d84e377725b1133843cb22
Surya Naga Sesank M <67413218+nagasesank@users.noreply.github.com>
2024-12-19 11:15:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "null_resource" "remote"{
 connection {
        type = "ssh"
        user = "ec2-user"
-       private_key = file("/home/surya/Documents/terraform/terraform1/ec2-user.pem")
+       private_key = file("ec2-user.pem")
        host  = aws_instance.app_server.public_dns
  }
 }
